7ee5e72911d8aa95b7fcea473a035d44899f76f3
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2022-04-26 18:26:44 +0800

    splash screen success

--- ChatApp_LLAMIDO/ChatApp_LLAMIDO.Android/MainActivity.cs
+++ ChatApp_LLAMIDO/ChatApp_LLAMIDO.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ChatApp_LLAMIDO.Droid
 {
-    [Activity(Label = "ChatApp_LLAMIDO", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ChatApp_LLAMIDO", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ ChatApp_LLAMIDO/ChatApp_LLAMIDO.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatApp_LLAMIDO.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash",
+            MainLauncher = true,
+            NoHistory =true,
+            Icon = "@drawable/pink")]
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            System.Threading.Thread.Sleep(500);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
