48b99391fccb4b78e5b13b0ff228579836a56bcf
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2022-04-26 16:59:48 +0800

    Add project files.

---
+++ ChatApp_LLAMIDO/ChatApp_LLAMIDO/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChatApp_LLAMIDO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
